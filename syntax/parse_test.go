c95327f5f7a684ee4256978e078cc74af9115dc2
James Jarvis <git@jamesjarvis.io>
2022-02-03 08:53:43 +0000

    more line breaks

--- syntax/parse_test.go
+++ syntax/parse_test.go
@@ -233,7 +233,9 @@ func TestTypeDefs(t *testing.T) {
 			`(DefStmt Name=f Params=(a TypeHint=(ListTypeHint Raw=list InnerTypeHint=(TupleTypeHint Raw=tuple InnerTypeHints=((LiteralTypeHint Raw=str Value=str) (LiteralTypeHint Raw=int Value=int))))) Body=((BranchStmt Token=pass)))`},
 		{`
 def f(
-	a: list[tuple[str, int]],
+	a: list[
+		tuple[str, int]
+	],
 ): pass`,
 			`(DefStmt Name=f Params=(a TypeHint=(ListTypeHint Raw=list InnerTypeHint=(TupleTypeHint Raw=tuple InnerTypeHints=((LiteralTypeHint Raw=str Value=str) (LiteralTypeHint Raw=int Value=int))))) Body=((BranchStmt Token=pass)))`},
 	} {
